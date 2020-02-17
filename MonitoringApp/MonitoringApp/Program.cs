20f7260642baba58343d7b4040fd8eb9fc8e9fbb
masoud <masoud.shayannejad@gmail.com>
2020-02-17 16:24:35 +0330

    first commit

---
+++ MonitoringApp/MonitoringApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using MonitoringLib;
+
+namespace MonitoringApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            
+            
+            // Console.WriteLine("arrays Processing. Please wait...");
+            //
+            // Recorder.Start();
+            //
+            // // simulate a process that requires some memory resources...
+            // int[] arrays = Enumerable.Range(1, 10_000).ToArray();
+            //
+            //
+            // // ...and takes some time to complete
+            // System.Threading.Thread.Sleep(new Random().Next(5, 10) * 1000);
+            //
+            // Recorder.Stop();
+            
+            
+            
+            ////////////////////////////////////////////////////////////////////////////////////////////
+            
+            
+            
+            int[] numbers = Enumerable.Range(1, 50_000).ToArray();
+            
+            
+            Recorder.Start();
+            Console.WriteLine("Using string with +");
+            string s = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                s += numbers[i] + ", ";
+            }
+
+            Recorder.Stop();
+            Recorder.Start();
+            Console.WriteLine("Using StringBuilder");
+            var builder = new System.Text.StringBuilder();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                builder.Append(numbers[i]);
+                builder.Append(", ");
+            }
+
+            Recorder.Stop();
+            
+            ////////////////////////////////////////////////////////////////////////////////////////////
+
+        }
+    }
+}
---
+++ MonitoringLib/MonitoringLib/Recorder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Diagnostics.Process;
+
+
+namespace MonitoringLib
+{
+    public class Recorder
+
+    {
+        static Stopwatch timer = new Stopwatch();
+        static long bytesPhysicalBefore = 0;
+        static long bytesVirtualBefore = 0;
+
+        
+        public static void Start()
+        {
+            // force two garbage collections to release memory that is
+            // no longer referenced but has not been released yet
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            bytesPhysicalBefore = GetCurrentProcess().WorkingSet64;
+            bytesVirtualBefore = GetCurrentProcess().VirtualMemorySize64;
+            
+            timer.Restart();
+
+        }
+
+
+        public static void Stop()
+        {
+            
+            timer.Stop();
+            
+           long  bytesPhysicalAfter = GetCurrentProcess().WorkingSet64;
+            long bytesVirtualAfter = GetCurrentProcess().VirtualMemorySize64;
+
+            Console.WriteLine($"physical byte used  : {(bytesPhysicalAfter -bytesPhysicalBefore):N0}");
+            Console.WriteLine($"virtual byte used  : {(bytesVirtualAfter -bytesVirtualBefore):N0}");
+            Console.WriteLine($"span time elapsed  : {timer.Elapsed}");
+            Console.WriteLine($"span time elapsed in milliseconds  : {timer.ElapsedMilliseconds:N0}");
+            
+            
+        }
+    }
+}
